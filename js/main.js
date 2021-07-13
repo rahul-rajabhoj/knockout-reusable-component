0a5ddd93553ecd0615ab4320350902bde73405b5
Rahul Rajabhoj <rrajabhoj@ml.local>
2021-07-13 17:10:58 +0530

    Added loading button component accepting params

--- js/main.js
+++ js/main.js
@@ -2,7 +2,21 @@ function ItemListViewModel() {
 
     var self = this;
 
-    self.items = ko.observableArray();
+    self.items = ko.observableArray([]);
 }
+
+ko.components.register("loading-button", {
+    template: [
+        '<button class="LoadingButton">',
+            '<span data-bind="text: buttonText"></span>',
+        '</button>',
+    ].join(''),
+    viewModel: function(params) {
+
+        var self = this;
+
+        self.buttonText = ko.observable(params.buttonText)
+    }
+});
 
 ko.applyBindings(new ItemListViewModel(), document.querySelector('#knockout-app'));
