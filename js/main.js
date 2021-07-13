413d5e4d67fe772d526c20448ce6647b75b2bb12
Rahul Rajabhoj <rrajabhoj@ml.local>
2021-07-13 17:27:24 +0530

    Added flag to handle loading state of button

--- js/main.js
+++ js/main.js
@@ -3,11 +3,22 @@ function ItemListViewModel() {
     var self = this;
 
     self.items = ko.observableArray([]);
+
+    self.fetchItems = function(callback) {
+        setTimeout(function() {
+            var MOCK_RESPONSE = {
+                items: [
+                    "I", "Found", "Some", "Items"
+                ]
+            }
+            callback(MOCK_RESPONSE);
+        }, 2000);
+    }
 }
 
 ko.components.register("loading-button", {
     template: [
-        '<button class="LoadingButton">',
+        '<button data-bind="click: onClick, css: { loading: isLoading }" class="LoadingButton">',
             '<span data-bind="text: buttonText"></span>',
         '</button>',
     ].join(''),
@@ -15,7 +26,17 @@ ko.components.register("loading-button", {
 
         var self = this;
 
-        self.buttonText = ko.observable(params.buttonText)
+        self.isLoading = ko.observable(false);
+
+        self.buttonText = ko.observable(params.buttonTitle);
+
+        self.onClick = function() {
+            self.isLoading(true);
+
+            params.action(function(data) {
+                self.isLoading(false);
+            });
+        }
     }
 });
 
